02e74272755d7a17bce690d40f5ec20ea3231d6a
marxlin <jiaqinlin12138@163.com>
2020-02-13 02:56:27 +0800

    delete

--- built/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("./src/db");
-var app_1 = require("./src/app");
-app_1.app.listen(3000);
-console.log("listen successfully");
-//# sourceMappingURL=main.js.map
--- built/src/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var targetUrl = "https://ehall.jnu.edu.cn/infoplus/form/XNYQSB/start";
-exports.targetUrl = targetUrl;
-var ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36";
-exports.ua = ua;
-var LOGINURL = "http://icas.jnu.edu.cn/cas/login";
-exports.LOGINURL = LOGINURL;
-//# sourceMappingURL=config.js.map
